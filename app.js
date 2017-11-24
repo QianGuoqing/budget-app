feea55b513ac07bedf5752d974cd834afeec7fd7
QianGuoqing <qian_guoqing@yeah.net>
2017-11-24 12:57:30 +0800

    add handle input data

--- app.js
+++ app.js
@@ -1,25 +1,64 @@
 var budgetController = (function() {
-  var x = 23
-  var add = function(a) {
-    return x + a
-  }
-
-  return {
-    publicTest: function(b) {
-      console.log(add(b))
-    }
-  }
+  
 })();
 
 var UIController = (function() {
 
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value,
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      }
+    },
+
+    getDomStrings: function() {
+      return DOMStrings
+    }
+  }
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5)
+
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDomStrings()
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem()
+      }
+    })
+  }
+
+  var ctrlAddItem = function() {
+    // 1. 获取输入数据
+    var input = UICtrl.getInput()
+    console.log(input)
+
+    // 2. 添加item至controller
+
+    // 3. 添加item至UI
+
+    // 4. 计算数据
+
+    // 5. UI上展现
+  }
+
   return {
-    anotherPublic: function() {
-      console.log(z)
+    init: function() {
+      setupEventListeners()
     }
   }
+
 })(budgetController, UIController);
+
+controller.init()
